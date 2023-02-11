2f092f4bb33e281efca3685dcd6b2da19c90382a
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2023-02-11 20:47:09 +0530

    student add_course working

---
+++ src/main/java/org/connection/Main.java
@@ -0,0 +1,43 @@
+package org.connection;
+import org.users.AcadOffice;
+import org.users.Faculty;
+import org.users.Student;
+import org.users.User;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // connection
+        Connect portal = new Connect();
+        Connection conn = portal.connect();
+
+        // get credentials and assign role
+        UI ui = new UI();
+        ArrayList<String> credentials = ui.getCredentials();
+        String role = credentials.get(2);
+        User user = new User(conn);
+        int id = user.login(credentials.get(0), credentials.get(1), role);
+        if(id == -1)
+        {
+            System.out.println("Invalid credentials!");
+            return;
+        }
+
+        // present menu depending on role
+        switch (role)
+        {
+            case "0":
+                user = new Student(conn, id);
+                ui.studentMenu((Student) user);
+                break;
+            case "1":
+                user = new Faculty(conn);
+                break;
+            case "2":
+                user = new AcadOffice(conn);
+                break;
+        }
+    }
+}
